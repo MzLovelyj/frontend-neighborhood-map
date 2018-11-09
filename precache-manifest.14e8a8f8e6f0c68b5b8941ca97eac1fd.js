52a119ccb12538bae6eb1e7fe9ac627873fb47d8
mzlovelyj <liljp064@gmail.com>
2018-11-09 15:00:19 -0500

    Updates

---
+++ precache-manifest.14e8a8f8e6f0c68b5b8941ca97eac1fd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "e7dda5140f3cd2f2fe545fe6affc9faa",
+    "url": "/frontend-neighborhood-map/static/media/foursquare.e7dda514.png"
+  },
+  {
+    "revision": "1cbd2e082cd207fc206418d0a4b17a5b",
+    "url": "/frontend-neighborhood-map/static/media/circles-loader.1cbd2e08.svg"
+  },
+  {
+    "revision": "9536873126dfa18c7b0b",
+    "url": "/frontend-neighborhood-map/static/js/runtime~main.95368731.js"
+  },
+  {
+    "revision": "615c4a5783d705465d35",
+    "url": "/frontend-neighborhood-map/static/js/main.615c4a57.chunk.js"
+  },
+  {
+    "revision": "f07a3633fc83cd730125",
+    "url": "/frontend-neighborhood-map/static/js/1.f07a3633.chunk.js"
+  },
+  {
+    "revision": "615c4a5783d705465d35",
+    "url": "/frontend-neighborhood-map/static/css/main.136b6151.chunk.css"
+  },
+  {
+    "revision": "441cd8da19d45b4842691c6214d1521c",
+    "url": "/frontend-neighborhood-map/index.html"
+  }
+];
